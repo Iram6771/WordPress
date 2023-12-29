d4bc50a413ce9c9e89b8d738239095a583845ce0
Iram6771 <bintmajeed529@gmail.com>
2023-12-29 13:08:35 +0500

    3rd assignment

---
+++ 3rdassignment.php
@@ -0,0 +1,15 @@
+<?php
+// 	Apply if and else condition and Find Greatest Number
+$num1st = 30;
+$num2nd = 60;
+$num3rd = 20
+
+if ($num1st>$num2nd && $num1st>$num3rd){
+    echo "30 is the gratest number";
+}
+else if($num2nd>$num3rd && $num2nd>$num1nd){
+    echo "60 is the gratest number";
+}
+else
+{echo "2o is the gratest number";}
+?>
